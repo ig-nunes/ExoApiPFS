2b208aba47fb663067f2ae7abffe6d9fdc74b642
Igor Nunes <inunes.us@gmail.com>
2022-07-27 10:07:56 -0300

    Implementacao dos metodos POST, PUT e DELETE

--- ExoApiPFS/Controllers/ProjetoController.cs
+++ ExoApiPFS/Controllers/ProjetoController.cs
@@ -1,4 +1,5 @@
-﻿using ExoApiPFS.Repositories;
+﻿using ExoApiPFS.Models;
+using ExoApiPFS.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,12 +17,102 @@ namespace ExoApiPFS.Controllers
             _projetoRepository = projetoRepository;
         }
 
+
         [HttpGet]
         public IActionResult ListarProjetos()
         {
             try
             {
                 return Ok(_projetoRepository.ListarProjetos());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Projeto projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+                if (projetoBuscado == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(projetoBuscado);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult CadastrarProjeto(Projeto projeto)
+        {
+            try
+            {
+                _projetoRepository.CadastrarProjeto(projeto);
+
+                return StatusCode(201);
+
+                //return Ok("Livro Cadastrado Com Sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarProjeto(int id)
+        {
+            try
+            {
+                Projeto projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+                if (projetoBuscado == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    _projetoRepository.DeletarProjeto(id);
+                }
+
+                return Ok("O Projeto foi removido com sucesso!");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult AlterarProjeto(int id, Projeto projeto)
+        {
+            try
+            {
+                Projeto projetoBuscado = _projetoRepository.BuscarPorId(id);
+
+                if (projetoBuscado == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    _projetoRepository.AtualizarProjeto(id, projeto);
+                }
+
+                return StatusCode(204);
             }
             catch (Exception e)
             {
--- ExoApiPFS/Repositories/ProjetoRepository.cs
+++ ExoApiPFS/Repositories/ProjetoRepository.cs
@@ -12,9 +12,55 @@ namespace ExoApiPFS.Repositories
             _context = context;
         }
 
+
         public List<Projeto> ListarProjetos()
         {
             return _context.Projetos.ToList();
+        }
+
+
+        public Projeto BuscarPorId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+
+        public void CadastrarProjeto(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+
+            _context.SaveChanges();
+        }
+
+
+        public void DeletarProjeto(int id)
+        {
+            Projeto projeto = _context.Projetos.Find(id);
+
+            if (projeto != null)
+            {
+                _context.Projetos.Remove(projeto);
+
+                _context.SaveChanges();
+            }
+        }
+
+
+        public void AtualizarProjeto(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.Titulo = projeto.Titulo;
+                projetoBuscado.Situacao = projeto.Situacao;
+                projeto.DataInicio = projeto.DataInicio;
+                projetoBuscado.Descricao = projeto.Descricao;
+            }
+
+            _context.Projetos.Update(projetoBuscado);
+
+            _context.SaveChanges();
         }
 
     }
